52119c898bba8611da077dc0f1df2e85b30c65ac
Rafael Cardoso <rscedf@gmail.com>
2022-12-19 14:22:52 -0300

    criando estados no componente

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,40 @@
+import { useState } from 'react'
+
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
 
 export default function HomePage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const handleLabelClick = () => {
     console.log('clicou aqui')
   }
+
   const handleSubmitForm = (event) => {
     event.preventDefault()
-    console.log('handle submit form')    
+    console.log('email: ', email, 'senha: ', password)
   }
-  const handleChangeInput = (event) => {
-    console.log(event.target.value)
+
+  const handleChangeInput = (event, onChange) => {
+    onChange(event.target.value)
   }
   return (
     <>
-      <Title>Formulário de login</Title>
+      <Title onClick={handleLabelClick}>Formulário de login</Title>
       <label onClick={handleLabelClick}>Voltar a página...</label>
       <form onSubmit={handleSubmitForm}>
-        <Input type="email" placeholder="Digite seu email" onChange = {handleChangeInput} />
-        <Input type="password" placeholder="Digite sua senha" onChange = {handleChangeInput} />
+        <Input
+          type="email"
+          placeholder="Digite seu email"
+          onChange={(e) => handleChangeInput(e, setEmail)}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={(e) => handleChangeInput(e, setPassword)}
+        />
         <Button type="submit">Botao dentro form</Button>
       </form>
       <label>Label Teste</label>
